9ba2efc06bce2770058bfc71be333c3e805ac431
mohamedelraghy <elraghy8@gmail.com>
2022-06-28 11:56:51 +0200

    make prgress at list posts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,9 +2,19 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-post-list',
-  templateUrl:'./post-list.component.html'
+  templateUrl:'./post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
 })
 
 export class PostListComponent{
+
+  // posts = [
+  //   {title: 'First Post', content: 'This is the First Post content'},
+  //   {title: 'second Post', content: 'This is the second Post content'},
+  //   {title: 'third Post', content: 'This is the third Post content'}
+  // ]
+
+  posts = [
+  ]
 
 }
