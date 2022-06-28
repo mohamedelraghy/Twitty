2fff0920e5c093d7547007b88a0660fdf02aaec5
mohamedelraghy <elraghy8@gmail.com>
2022-06-28 11:06:21 +0200

    create post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreatePostComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/create-post/create-post.component.ts
+++ src/app/posts/create-post/create-post.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-create-post',
-  templateUrl: './create-post.component.html'
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.css'],
 })
 
 export class CreatePostComponent {
