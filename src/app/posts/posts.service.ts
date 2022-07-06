6e00e04fb4f589db8d6468125d1dbbea4e88ffad
mohamedelraghy <elraghy8@gmail.com>
2022-07-06 18:02:09 +0200

    fetch posts from the server and stor it in posts list

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { Subject } from 'rxjs'
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({providedIn: 'root'})
@@ -8,8 +9,14 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts(){
-    return [...this.posts];
+    this.http.get<{messege : string, posts: Post[]}>('http://localhost:3000/api/posts')
+    .subscribe((postsData) => {
+      this.posts = postsData.posts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   getPostUpdateListner(){
@@ -17,7 +24,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string){
-    const post: Post = {title: title, content: content}
+    const post: Post = { id: '1', title: title, content: content }
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
