319f8a8dc211fd8de75e3facc9df39c12be29891
mohamedelraghy <elraghy8@gmail.com>
2022-07-06 18:25:27 +0200

    solve Cors problem by adding headers

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, POTIONS"
+  );
+  next();
+})
 
 app.use('/api/posts', (req, res, next) => {
   const posts = [
