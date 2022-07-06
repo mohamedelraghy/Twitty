c500ef08bc505320ffef5218dc7dc6e5f8a6f4c4
mohamedelraghy <elraghy8@gmail.com>
2022-07-06 18:32:54 +0200

    use body Parser

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
