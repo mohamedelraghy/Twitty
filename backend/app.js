d8e62ce5af6dbab2e6cde33c4e2d4704696e6a3e
mohamedelraghy <elraghy8@gmail.com>
2022-07-06 17:07:11 +0200

    fetch posts route

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,18 @@ const express = require('express');
 const app = express();
 
 
-app.use((req, res, next) => {
-  console.log('First Middelware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!')
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'srgladsf',
+      title: 'Hello',
+      content: "Hello from the other side"
+    }
+  ];
+  return res.status(200).json({
+    messege: "Posts fetched succesfully!",
+    posts: posts
+  });
 });
 
 module.exports = app;
