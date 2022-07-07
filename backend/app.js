fcc0dbc45d829c693c7dd35d5eb89ea27fab732d
mohamedelraghy <elraghy8@gmail.com>
2022-07-07 10:07:12 +0200

    exports post module

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
-73PIVhPYvBWMasSo
+// 73PIVhPYvBWMasSo
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://Moelraghy:73PIVhPYvBWMasSo@messageme.c12pk.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('DB Connected Successfully');
+  }).catch(() => {
+    console.log('DB connection Failed');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,8 +31,13 @@ app.use((req, res, next) => {
 })
 
 app.post('/api/posts', (req, res, next) =>{
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+
   console.log(post);
+
   res.status(201).json({
     messege : 'Post added Successfully',
   });
