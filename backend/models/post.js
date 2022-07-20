686556d85fc4a43c40ff35ee732b56c82fa1e702
mohamedelraghy <elraghy8@gmail.com>
2022-07-20 16:28:02 +0200

    add imagePath proprty to post module

--- backend/models/post.js
+++ backend/models/post.js
@@ -8,7 +8,11 @@ const postSchema = mongoose.Schema({
   content : {
     type: String,
     required: true
-  }
+  },
+  imagePath: {
+    type: String,
+    required: true
+  },
 });
 
 module.exports = mongoose.model('Post', postSchema);
