1eef795b0cc629d127244ec7d9d98e674d3c87e8
mohamedelraghy <elraghy8@gmail.com>
2022-07-20 18:08:24 +0200

    update imagev

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -68,12 +68,19 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', multer({storage: storage}).single("image"), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if(req.file) {
+    const url = req.protocol + '://' + req.get('host');
+    imagePath = url + "/images/" + req.file.filename
+  }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    connect: req.body.connect
+    connect: req.body.connect,
+    imagePath: imagePath
   });
+  
   Post.updateOne({_id: req.params.id}, post).then( result => {
     res.status(200).json({
       messege: "post update successfully"
