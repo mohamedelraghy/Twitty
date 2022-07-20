f2f1b6639f1a51e6f8df8ebc0f7787967b541697
mohamedelraghy <elraghy8@gmail.com>
2022-07-20 21:06:22 +0200

    implemnt pagination logic

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -23,6 +23,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   isLoading = false;
   totalPosts = 10;
   postsPerPage = 2;
+  currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription = new Subscription();
 
@@ -30,7 +31,7 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnInit(){
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService.getPostUpdateListner()
     .subscribe((posts: Post[]) => {
       this.isLoading = false;
@@ -39,8 +40,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   onChangedPage(pageData: PageEvent){
-    console.log(pageData);
-
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
 
   onDelete(postId: string){
