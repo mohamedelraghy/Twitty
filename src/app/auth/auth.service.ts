15ca5c29e9eab64dc5d237637b1008ab160053cb
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 20:31:39 +0200

    return authStatus

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,24 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 import { AuthData } from "./auth-data.model";
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {};
 
   getToken(){
-    return this.token; 
+    return this.token;
   }
+
+  getAuthStatusListner(){
+    return this.authStatusListener.asObservable();
+  }
+
   createUser(email: string, password: string){
     const authData : AuthData = {
       email: email,
@@ -32,6 +39,7 @@ export class AuthService {
       .subscribe(response => {
         const token = response.token;
         this.token = token;
+        this.authStatusListener.next(true);
       });
   }
 }
