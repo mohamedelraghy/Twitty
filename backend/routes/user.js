04dc3034d8c57d4cf1ca0a5588a583e14fb50e33
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 15:59:21 +0200

    create a route that make a token

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -25,6 +26,32 @@ router.post("/signup", (req, res, next)=> {
         })
       });
   });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user =>{
+      if(!user) return res.status(401).json({
+        message: "Auth failded"
+      });
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if(!result){
+        return res.status(401).json({
+          message: "Auth failded"
+        });
+      }
+      const token = jwt.sign(
+        { email: user.email, userId: user._id },
+        'secret_this_should_be_longer',
+        {expiresIn: '1h'});
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failded"
+      });
+    });
 });
 
 module.exports = router;
