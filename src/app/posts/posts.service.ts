c8794a7990d348ab7cd75145712fa059b7a8a1b8
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 11:17:37 +0200

    finishin paginator

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -59,16 +59,8 @@ export class PostsService {
         'http://localhost:3000/api/posts',
          postData
       )
-      .subscribe((responseData) =>{
-        const post: Post = {
-          id: responseData.post.id,
-          title: title,
-          content: content,
-          imagePath: responseData.post.imagePath,
-        };
+      .subscribe(responseData =>{
 
-        this.posts.push(post);
-        this.postsUpdated.next([...this.posts]);
         this.router.navigate(['/']);
       });
   }
@@ -97,27 +89,12 @@ export class PostsService {
     this.http
       .put('http://localhost:3000/api/posts/' + postId, postData)
       .subscribe(response =>{
-        const updatedPosts = [...this.posts];
-        const oldPostIndex = updatedPosts.findIndex(p => p.id === postId);
-        const post: Post = {
-        id: postId,
-        title: title,
-        content: content,
-        imagePath: ''
-      }
-        updatedPosts[oldPostIndex] = post;
-        this.posts = updatedPosts;
-        this.postsUpdated.next([...this.posts]);
+
         this.router.navigate(['/']);
       });
   }
 
   deletePost(postId: string){
-      this.http.delete('http://localhost:3000/api/posts/' + postId)
-        .subscribe( () => {
-          const updatedPosts = this.posts.filter(post => post.id !== postId);
-          this.posts = updatedPosts;
-          this.postsUpdated.next([...this.posts]);
-        });
+      return this.http.delete('http://localhost:3000/api/posts/' + postId);
   }
 }
