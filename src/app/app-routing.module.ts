b169f5d3ea5b03f86514fe83b619cca147157e82
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 12:18:37 +0200

    add login url to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: CreatePostComponent },
-  { path: 'edit/:postId', component: CreatePostComponent }
+  { path: 'edit/:postId', component: CreatePostComponent },
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
