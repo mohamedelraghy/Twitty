23460e62fc2e77f1b2ea933dd5a7064d334e4448
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 13:45:57 +0200

    gave a url to userRoute

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -32,7 +33,8 @@ app.use((req, res, next) => {
   next();
 })
 
-app.use("/api/posts",postRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/user", userRoutes);
 
 
 module.exports = app;
