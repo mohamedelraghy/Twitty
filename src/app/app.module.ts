ae4d2e21ab64ffacfd3d9c9e7523490eacebeee6
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 11:47:06 +0200

    create login component and import it to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { AppComponent } from './app.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { LoginComponent } from './auth/login/login.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     AppComponent,
     CreatePostComponent,
     HeaderComponent,
-    PostListComponent
+    PostListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
