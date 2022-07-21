1cc9f897344370204356f420d3dbf0274dcb7dfb
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 13:20:00 +0200

    fix some typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { CreatePostComponent } from './posts/create-post/create-post.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { LoginComponent } from './auth/login/login.component';
-import { SingupComponent } from './auth/singup/singup.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 @NgModule({
@@ -30,7 +30,7 @@ import { SingupComponent } from './auth/singup/singup.component';
     HeaderComponent,
     PostListComponent,
     LoginComponent,
-    SingupComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
