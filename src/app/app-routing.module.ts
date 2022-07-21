d93740052159dd41cac4524c37bfeb88ad0b7e72
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 21:36:43 +0200

    add guard to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
@@ -7,14 +8,15 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
-  { path: 'create', component: CreatePostComponent },
-  { path: 'edit/:postId', component: CreatePostComponent },
+  { path: 'create', component: CreatePostComponent, canActivate:[AuthGuard] },
+  { path: 'edit/:postId', component: CreatePostComponent, canActivate:[AuthGuard] },
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
