3e01637444a667f46562dea764a83eb079938276
mohamedelraghy <elraghy8@gmail.com>
2022-07-21 13:19:31 +0200

    add signup route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: CreatePostComponent },
   { path: 'edit/:postId', component: CreatePostComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
